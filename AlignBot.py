e3e6a3580113fd43fb10aedae103f971fc1d5131
Evan Alioto <EvanJAlioto@gmail.com>
2021-04-27 23:39:14 -0700

    set bot to scrape posts that only have more than 100 upvotes

--- AlignBot.py
+++ AlignBot.py
@@ -13,7 +13,7 @@ token = open("token.txt", "r").read()
 
 def bot_login():
     print("Loggin in.")
-    r = praw.Reddit(username=config.username, password=config.password, client_id=config.client_id,
+    r = praw.Reddit(username=config.username, password=config.password, client_id=config.client_id,  # reddit login
                     client_secret=config.client_secret, user_agent=config.user_agent)
     print("Logged in.")
     return r
@@ -28,57 +28,58 @@ def run_bot(r):
         for submission in submissions:
             if submission not in blacklist:
                 if submission.is_self == False and submission.is_video == False:
-                    data = {}
-                    data["embeds"] = []
-                    embed = {
-                        "title": config.embed_title,
-                        "author": {
-                            "name": "posted by u/{} at r/{}".format(str(submission.author), str(submission.subreddit)),
-                            "url": "https://reddit.com/r/{}/comments/{}".format(str(submission.subreddit), str(submission)),
-                            "icon_url": submission.author.icon_img
-                        },
-                        "image": {
-                            "url": submission.url
-                        },
-                        "fields": [
-                            {
-                                "name": "Post Title",
-                                "value": submission.title,
-                                "inline": False
+                    if submission.score > 100:
+                        data = {}
+                        data["embeds"] = []
+                        embed = {
+                            "title": config.embed_title,
+                            "author": {
+                                "name": "posted by u/{} at r/{}".format(str(submission.author), str(submission.subreddit)),
+                                "url": "https://reddit.com/r/{}/comments/{}".format(str(submission.subreddit), str(submission)),
+                                "icon_url": submission.author.icon_img
                             },
-                            {
-                                "name": "Author",
-                                "value": "u/{}".format(str(submission.author)),
-                                "inline": True
+                            "image": {
+                                "url": submission.url
                             },
-                            {
-                                "name": "Subreddit",
-                                "value": "r/{}".format(str(submission.subreddit)),
-                                "inline": True
-                            },
-                            {
-                                "name": "Score",
-                                "value": submission.score,
-                                "inline": False
-                            }
-                        ]
-                    }
-                    data["embeds"].append(embed)
+                            "fields": [
+                                {
+                                    "name": "Post Title",
+                                    "value": submission.title,
+                                    "inline": False
+                                },
+                                {
+                                    "name": "Author",
+                                    "value": "u/{}".format(str(submission.author)),
+                                    "inline": True
+                                },
+                                {
+                                    "name": "Subreddit",
+                                    "value": "r/{}".format(str(submission.subreddit)),
+                                    "inline": True
+                                },
+                                {
+                                    "name": "Score",
+                                    "value": submission.score,
+                                    "inline": False
+                                }
+                            ]
+                        }
+                        data["embeds"].append(embed)
 
-                    req = requests.post(config.webhook_url, data=json.dumps(
-                        data), headers={"Content-Type": "application/json"})
+                        req = requests.post(config.webhook_url, data=json.dumps(
+                            data), headers={"Content-Type": "application/json"})
 
-                    if req.status_code == 204:
-                        print(
-                            "Found new hot Reddit post, sending it to Discord.")
+                        if req.status_code == 204:
+                            print(
+                                "Found new hot Reddit post, sending it to Discord.")
 
-                    blacklist.append(submission)
-                    with open("blacklist.txt", "a") as f:
-                        f.write(str(submission) + "\n")
-                        # f.close()
+                        blacklist.append(submission)
+                        with open("blacklist.txt", "a") as f:
+                            f.write(str(submission) + "\n")
+                            # f.close()
 
-                    time.sleep(config.wait_time)
-                    run_bot(r)
+                        time.sleep(config.wait_time)
+                        run_bot(r)
 
 
 def outriders(r):
@@ -87,58 +88,59 @@ def outriders(r):
         for submission in submissions:
             if submission not in blacklist:
                 if submission.is_self == False and submission.is_video == False:
-                    data = {}
-                    data["embeds"] = []
-                    embed = {
-                        "title": config.embed_title,
-                        "author": {
-                            "name": "posted by u/{} at r/{}".format(str(submission.author), str(submission.subreddit)),
-                            "url": "https://reddit.com/r/{}/comments/{}".format(str(submission.subreddit), str(submission)),
-                            "icon_url": submission.author.icon_img
-                        },
-                        "image": {
-                            "url": submission.url
-                        },
-                        "fields": [
-                            {
-                                "name": "Post Title",
-                                "value": submission.title,
-                                "inline": False
+                    if submission.score > 100:
+                        data = {}
+                        data["embeds"] = []
+                        embed = {
+                            "title": config.embed_title,
+                            "author": {
+                                "name": "posted by u/{} at r/{}".format(str(submission.author), str(submission.subreddit)),
+                                "url": "https://reddit.com/r/{}/comments/{}".format(str(submission.subreddit), str(submission)),
+                                "icon_url": submission.author.icon_img
                             },
-                            {
-                                "name": "Author",
-                                "value": "u/{}".format(str(submission.author)),
-                                "inline": True
+                            "image": {
+                                "url": submission.url
                             },
-                            {
-                                "name": "Subreddit",
-                                "value": "r/{}".format(str(submission.subreddit)),
-                                "inline": True
-                            },
-                            {
-                                "name": "Score",
-                                "value": submission.score,
-                                "inline": False
-                            }
-                        ]
-                    }
-                    data["embeds"].append(embed)
+                            "fields": [
+                                {
+                                    "name": "Post Title",
+                                    "value": submission.title,
+                                    "inline": False
+                                },
+                                {
+                                    "name": "Author",
+                                    "value": "u/{}".format(str(submission.author)),
+                                    "inline": True
+                                },
+                                {
+                                    "name": "Subreddit",
+                                    "value": "r/{}".format(str(submission.subreddit)),
+                                    "inline": True
+                                },
+                                {
+                                    "name": "Score",
+                                    "value": submission.score,
+                                    "inline": False
+                                }
+                            ]
+                        }
+                        data["embeds"].append(embed)
 
-                    req = requests.post(config.outriders_webhook_url, data=json.dumps(
-                        data), headers={"Content-Type": "application/json"})
+                        req = requests.post(config.outriders_webhook_url, data=json.dumps(
+                            data), headers={"Content-Type": "application/json"})
 
-                    if req.status_code == 204:
-                        print(
-                            "Found new hot Reddit post, sending it to Discord.")
+                        if req.status_code == 204:
+                            print(
+                                "Found new hot Reddit post, sending it to Discord.")
 
-                    blacklist.append(submission)
-                    with open("blacklist.txt", "a") as f:
-                        f.write(str(submission) + "\n")
-                        # f.close()
+                        blacklist.append(submission)
+                        with open("blacklist.txt", "a") as f:
+                            f.write(str(submission) + "\n")
+                            # f.close()
 
-                    time.sleep(config.wait_time)
-                    # print('Functions are calling themselves every 3 mins')
-                    # outriders(r)
+                        time.sleep(config.wait_time)
+                        # print('Functions are calling themselves every 3 mins')
+                        # outriders(r)
 
 
 def blacklisted_posts():
