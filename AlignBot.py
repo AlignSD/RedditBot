3172b825e95d9c403a1eff91c14d3a56b66098fc
Evan Alioto <EvanJAlioto@gmail.com>
2021-08-22 20:25:08 -0700

    removed outriders bot cause the game was crap and nobody cares about it anymore

--- AlignBot.py
+++ AlignBot.py
@@ -82,65 +82,65 @@ def run_bot(r):
                         run_bot(r)
 
 
-def outriders(r):
-    for sub in config.outriders:
-        submissions = r.subreddit(sub).hot(limit=config.limit)
-        for submission in submissions:
-            if submission not in blacklist:
-                if submission.is_self == False and submission.is_video == False:
-                    if submission.score > 900:
-                        data = {}
-                        data["embeds"] = []
-                        embed = {
-                            "title": config.embed_title,
-                            "author": {
-                                "name": "posted by u/{} at r/{}".format(str(submission.author), str(submission.subreddit)),
-                                "url": "https://reddit.com/r/{}/comments/{}".format(str(submission.subreddit), str(submission)),
-                                "icon_url": submission.author.icon_img
-                            },
-                            "image": {
-                                "url": submission.url
-                            },
-                            "fields": [
-                                {
-                                    "name": "Post Title",
-                                    "value": submission.title,
-                                    "inline": False
-                                },
-                                {
-                                    "name": "Author",
-                                    "value": "u/{}".format(str(submission.author)),
-                                    "inline": True
-                                },
-                                {
-                                    "name": "Subreddit",
-                                    "value": "r/{}".format(str(submission.subreddit)),
-                                    "inline": True
-                                },
-                                {
-                                    "name": "Score",
-                                    "value": submission.score,
-                                    "inline": False
-                                }
-                            ]
-                        }
-                        data["embeds"].append(embed)
+# def outriders(r):
+#     for sub in config.outriders:
+#         submissions = r.subreddit(sub).hot(limit=config.limit)
+#         for submission in submissions:
+#             if submission not in blacklist:
+#                 if submission.is_self == False and submission.is_video == False:
+#                     if submission.score > 900:
+#                         data = {}
+#                         data["embeds"] = []
+#                         embed = {
+#                             "title": config.embed_title,
+#                             "author": {
+#                                 "name": "posted by u/{} at r/{}".format(str(submission.author), str(submission.subreddit)),
+#                                 "url": "https://reddit.com/r/{}/comments/{}".format(str(submission.subreddit), str(submission)),
+#                                 "icon_url": submission.author.icon_img
+#                             },
+#                             "image": {
+#                                 "url": submission.url
+#                             },
+#                             "fields": [
+#                                 {
+#                                     "name": "Post Title",
+#                                     "value": submission.title,
+#                                     "inline": False
+#                                 },
+#                                 {
+#                                     "name": "Author",
+#                                     "value": "u/{}".format(str(submission.author)),
+#                                     "inline": True
+#                                 },
+#                                 {
+#                                     "name": "Subreddit",
+#                                     "value": "r/{}".format(str(submission.subreddit)),
+#                                     "inline": True
+#                                 },
+#                                 {
+#                                     "name": "Score",
+#                                     "value": submission.score,
+#                                     "inline": False
+#                                 }
+#                             ]
+#                         }
+#                         data["embeds"].append(embed)
 
-                        req = requests.post(config.outriders_webhook_url, data=json.dumps(
-                            data), headers={"Content-Type": "application/json"})
+#                         req = requests.post(config.outriders_webhook_url, data=json.dumps(
+#                             data), headers={"Content-Type": "application/json"})
 
-                        if req.status_code == 204:
-                            print(
-                                "Found new hot Reddit post, sending it to Discord.")
+#                         if req.status_code == 204:
+#                             print(
+#                                 "Found new hot Reddit post, sending it to Discord.")
 
-                        blacklist.append(submission)
-                        with open("blacklist.txt", "a") as f:
-                            f.write(str(submission) + "\n")
-                            # f.close()
+#                         blacklist.append(submission)
+#                         with open("blacklist.txt", "a") as f:
+#                             f.write(str(submission) + "\n")
+#                             # f.close()
 
-                        time.sleep(config.wait_time)
-                        # print('Functions are calling themselves every 3 mins')
-                        # outriders(r)
+#                         time.sleep(config.wait_time)
+#                         # print('Functions are calling themselves every 3 mins')
+#                         # outriders(r)
 
 
 def blacklisted_posts():
